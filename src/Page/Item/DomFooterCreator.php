9ea363b09110eb24adf135be536d5a394fce0645
Your Name <you@example.com>
2015-11-25 12:32:37 +0100

    Footer creator as DOM

---
+++ src/Page/Item/DomFooterCreator.php
@@ -0,0 +1,57 @@
+<?php
+namespace TygrolewGmail\Page\Item;
+
+class DomFooterCreator implements IFooterCreator
+{
+  private $author;
+  private $year;
+  private $footer;
+  private $footerContent;
+
+  public function __construct($author = null, $year = null)
+  {
+    $this->author = $author;
+    $this->year = $year;
+    $this->createFooter();
+  }
+
+  public function getFooter()
+  {
+    return $this->footer;
+  }
+
+  protected function createFooter()
+  {
+    $this->createFooterContent();
+
+    $dom  = new DOMDocument();
+    $frag = $dom->createDocumentFragment();
+    $frag->appendXML('<hr/>');
+    $div  = $dom->createElement('div');
+    $text = $dom->createTextNode( $this->footerContent );
+    $attr = $dom->createAttribute('class');
+    $attr->value = "footer";
+    $div->appendChild($text);
+    $div->appendChild($attr);
+    $frag->appendChild($div);
+    $dom->appendChild($frag);
+
+    $this->footer = $dom->saveHTML();
+  }
+
+  protected function createFooterContent()
+  {
+    if ($this->year !== null) {
+      $year = $this->year;
+    }
+    else {
+      $year = date("Y"); // current year
+    }
+    if ($this->author !== null) {
+      $this->footerContent = "&copy; " . $year . " ". $this->author;
+    }
+    else {
+      $this->footerContent = "Footer";
+    }
+  }
+}
